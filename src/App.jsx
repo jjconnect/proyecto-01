77e3fe29a0ed9c171dde1bc6b1e2e69ebe64267d
jjconnect <jjconnect@gmail.com>
2021-01-17 18:41:21 -0500

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Contacto from './components/Contacto';
 import Footers from './components/Footers';
 import Inicio from './components/Inicio';
 import Navbar from './components/Navbar';
 import QuienesSomos from './components/QuienesSomos';
 import Servicios from './components/Servicios';
-import Panel from './components/Panel'
 
 function App() {
   return (
